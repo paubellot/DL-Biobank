e1549d1f9290d3100130cbc1443b4bbf5d92fd20
Pau Bellot <pau.bellot@gmail.com>
2018-03-16 16:22:35 +0100

    Update sim.sh

--- simulation/sim.sh
+++ simulation/sim.sh
@@ -1,7 +1,64 @@
-#source biobank/bin/activate
-for i in $(seq 1 2); do
-    time python main.py --genmodel linear --loci 100 --k 10000 --method lasso --recreation True
-    python main.py --genmodel linear --loci 100 --k 10000 --method ridge 
-    python main.py --genmodel linear --loci 100 --k 10000 --method mlp1
-    python main.py --genmodel linear --loci 100 --k 10000 --method mlp2 
+#!/bin/bash
+source ~/Projects/biobank/biobank/bin/activate
+model=epistasia
+model=linear
+nloci=1000
+for i in $(seq 1 10); do
+    k=10000
+    time python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso --recreation True
+    for k in 20000 50000; do
+        python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso
+    done
+    for k in 10000 20000 50000; do
+        for method in ridge mlp1 mlp2; do
+            python main.py  --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method $method 
+        done
+    done
+done
+exit
+nloci=1000
+for i in $(seq 1 10); do
+    k=10000
+    time python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso --recreation True
+    for k in 20000 50000; do
+        python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso
+    done
+    for k in 10000 20000 50000; do
+        for method in ridge mlp1 mlp2; do
+            python main.py  --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method $method
+        done
+    done
+done
+
+exit
+model=linear
+nloci=5000
+for i in $(seq 1 10); do
+    k=10000
+    time python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso --recreation True
+    for k in 20000 50000; do
+        python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso
+    done
+    for k in 10000 20000 50000; do
+        for method in ridge mlp1 mlp2; do
+            python main.py  --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method $method 
+        done
+    done
+done
+
+### new gamma
+model=linear
+nloci=5000
+for g in $(seq 1 10); do
+    i=g$g
+    k=10000
+    time python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --method lasso --shape 10 --recreation True
+    for k in 20000 50000; do
+        python main.py --id $i$model$nloci --genmodel $model --loci $nloci --k $k --shape 10 --method lasso
+    done
+    for k in 10000 20000 50000; do
+        for method in ridge mlp1 mlp2; do
+            python main.py  --id $i$model$nloci --genmodel $model --loci $nloci --k $k --shape 10 --method $method 
+        done
+    done
 done
